64b9d2a641d85a01fa17dd3203d5c34f42c785ac
sherulfernando2000 <sherul.dhanushka@gmail.com>
2025-03-24 01:21:48 +0530

    1.8 side bar load done

--- classroom-backend/classroom-backend/src/main/java/lk/ijse/classroombackend/repo/UserRepository.java
+++ classroom-backend/classroom-backend/src/main/java/lk/ijse/classroombackend/repo/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface gUserRepository extends JpaRepository<User,String> {
+public interface UserRepository extends JpaRepository<User,String> {
 
     User findByEmail(String userName);
 
